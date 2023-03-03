da7c5f2ca42fc24cba22721fd1fc50089a51c968
JulianIgnacio <montenegrojulianig@gmail.com>
2023-03-02 22:48:51 -0300

    realizando ejercicio 1

---
+++ README.md
@@ -0,0 +1,9 @@
+# Trabajo Practico 2 js
+
+## Descripcion
+
+Aqui se encuentran realizados los ejercicios del trabajo practico de conceptos de javascript
+
+## Autor
+
+1.Julian Montenegro
---
+++ js/Ejercicio 1.js
@@ -0,0 +1,7 @@
+let edad = parseInt(prompt('ingrese la edad'));
+
+if(edad >=18){
+    document.write('Puede Conducir');
+}else {
+    document.write('No posee la edad adecuada para conducir');
+}
