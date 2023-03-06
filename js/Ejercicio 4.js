e3104414782c85f9efdb35f1e6b33252bf6d6df9
JulianIgnacio <montenegrojulianig@gmail.com>
2023-03-05 22:04:54 -0300

    bloqueado en ejercicio 4

--- js/Ejercicio 4.js
+++ js/Ejercicio 4.js
@@ -2,7 +2,10 @@ let sumatoria = 0;
 
 while(confirm('Le interesa realizar una suma')=== true){
 let numero = parseInt(prompt('Ingrese un numero'));
-if(numero === Number){
-    
+if((typeof numero) === number){
+    sumatoria = sumatoria + numero;
+}else{
+    alert('No es un numero');
 }
 }
+document.write(sumatoria);
