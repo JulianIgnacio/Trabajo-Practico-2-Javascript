42a5b5c0bb5dfb18829de02c504f3a9c2fee86f1
JulianIgnacio <montenegrojulianig@gmail.com>
2023-03-07 21:13:39 -0300

    revisar ejercicio 4

--- js/Ejercicio 4.js
+++ js/Ejercicio 4.js
@@ -1,9 +1,9 @@
 let sumatoria = 0;
 
 while(confirm('Le interesa realizar una suma')=== true){
-let numero = parseInt(prompt('Ingrese un numero'));
-if(typeof numero === 'number'){
-    sumatoria = sumatoria + numero;
+ let numero = prompt('Ingrese un numero');
+ if(typeof numero == 'number'){
+    parseint(sumatoria) = sumatoria + numero;
 }else {
     alert('No es un numero');
 } 
